6bb36c57950c656215dd10a99ad2f05497ac3add
Sergei Bondarenko <andbondsa@gmail.com>
2020-02-10 10:07:57 +0500

    refactor: move tokens mapping to the token main parser

--- src/tokenize.js
+++ src/tokenize.js
@@ -1,39 +1,39 @@
 import P from './lib/parser';
 import { getSortedOperationsLexemes } from './operations';
 
-const SPACES = P.many1(P.char(' '))
-  .map((val) => ({ type: 'space', value: val.join('') }));
+const SPACES = P.many1(P.char(' '));
 
-const OPAREN = P.char('(')
-  .map((value) => ({ type: 'openParenthesis', value }));
+const OPAREN = P.char('(');
 
-const CPAREN = P.char(')')
-  .map((value) => ({ type: 'closeParenthesis', value }));
+const CPAREN = P.char(')');
 
 const DIGIT = P.anyFromString('0123456789');
 
-const INT = P.many1(DIGIT)
-  .map((digits) => digits.join(''));
+const INT = P.many1(DIGIT);
 
 const SEPARATOR = P.char('.');
 
 const FLOAT = P.many1(DIGIT)
   .chain((intPart) => SEPARATOR.chain((sep) => P.many1(DIGIT)
-    .map((fractPart) => [...intPart, sep, ...fractPart].join(''))));
+    .map((fractPart) => [...intPart, sep, ...fractPart])));
 
-const NUM = FLOAT.or(INT)
-  .map((value) => ({ type: 'number', value }));
+const NUM = FLOAT.or(INT);
 
 const opsLexs = getSortedOperationsLexemes();
-const OP = P.any(opsLexs.map(P.char))
-  .map((value) => ({ type: 'operator', value }));
+const OP = P.any(opsLexs.map(P.char));
 
 export default (input) => {
   if (input === '') {
     return [];
   }
 
-  const TOKENS = P.any(SPACES, NUM, OP, OPAREN, CPAREN);
+  const TOKENS = P.any(
+    SPACES.map((val) => ({ type: 'space', value: val.join('') })),
+    NUM.map((val) => ({ type: 'number', value: val.join('') })),
+    OP.map((value) => ({ type: 'operator', value })),
+    OPAREN.map((value) => ({ type: 'openParenthesis', value })),
+    CPAREN.map((value) => ({ type: 'closeParenthesis', value }))
+  );
   const tokenizer = P.many1(TOKENS);
   const parsed = tokenizer.parse(input);
 
