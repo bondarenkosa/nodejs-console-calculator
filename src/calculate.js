9b73d1c0dbad9d2a3fa60bd502180d096e9aebac
Sergei Bondarenko <andbondsa@gmail.com>
2020-02-11 14:37:00 +0500

    refactor: add method creating expression parser

--- src/calculate.js
+++ src/calculate.js
@@ -1,5 +1,5 @@
 import P from './lib/parser';
-import { getOperationsByType, getBinaryOperationsByPrecedence } from './operations';
+import getOperations from './operations';
 
 const makeTokenParserByType = (requiredType) => P.token(({ type }) => requiredType === type);
 
@@ -8,17 +8,50 @@ const makeOperatorParser = (opName, opFn) => P.token(({ type, value }) => (
 ))
   .map(() => opFn);
 
-const makeOperationsParser = (ops) => P.any(
+const makeOperatorsParser = (ops) => P.any(
   ops.map((op) => makeOperatorParser(op.operator, op.fn)),
 );
 
-const makeUnaryOperationChain = (term, unaryOp) => unaryOp.chain((opFn) => term
-  .map((x) => opFn(x)));
+const makeUnaryOperationChain = (term, unaryOp) => unaryOp
+  .chain((opFn) => term
+    .map((x) => opFn(x)));
 
 const makeBinaryOperationChain = (term1, term2, binaryOp) => term1
-  .chain((x) => binaryOp.chain((opFn) => term2
-    .map((y) => opFn(x, y))))
-  .or(term1);
+  .chain((x) => binaryOp
+    .chain((opFn) => term2
+      .map((y) => opFn(x, y))));
+
+const makeExpressionParser = (initialParser) => {
+  const operations = getOperations();
+
+  const sorted = operations.sort((a, b) => {
+    if (a.precedence === b.precedence) {
+      return 0;
+    }
+    return a.precedence < b.precedence ? 1 : -1;
+  });
+
+  const opsWithParsers = sorted.map((op) => ({ ...op, parser: makeOperatorsParser(op.operators) }));
+  const exprParser = opsWithParsers.reduce((acc, { type, parser: opsParser }) => {
+    let chain;
+    switch (type) {
+      case 'unary':
+        chain = makeUnaryOperationChain(P.lazy(() => chain), opsParser)
+          .or(acc);
+        break;
+      case 'binary':
+        chain = makeBinaryOperationChain(acc, P.lazy(() => chain), opsParser)
+          .or(acc);
+        break;
+      default:
+        throw new SyntaxError(`Unexpected operation type: ${type}`);
+    }
+
+    return chain;
+  }, initialParser);
+
+  return exprParser;
+};
 
 
 const NUM = makeTokenParserByType('number');
@@ -26,18 +59,6 @@ const NUM = makeTokenParserByType('number');
 const OPAREN = makeTokenParserByType('openParenthesis');
 
 const CPAREN = makeTokenParserByType('closeParenthesis');
-
-const UNARY_OPS = makeOperationsParser(getOperationsByType('unary'));
-
-/**
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
- *
- * Operator precedence determines how operators are parsed concerning each other.
- * Operators with higher precedence become the operands of operators with lower precedence.
- */
-const BINARY_OPS_PR15 = makeOperationsParser(getBinaryOperationsByPrecedence(15));
-const BINARY_OPS_PR14 = makeOperationsParser(getBinaryOperationsByPrecedence(14));
-const BINARY_OPS_PR13 = makeOperationsParser(getBinaryOperationsByPrecedence(13));
 
 const FACTOR = P.any(
   NUM.map(({ value }) => Number(value)),
@@ -47,15 +68,7 @@ const FACTOR = P.any(
       .map(() => exp))),
 );
 
-const T1 = makeUnaryOperationChain(P.lazy(() => T1), UNARY_OPS)
-  .or(FACTOR);
-
-const T2 = makeBinaryOperationChain(T1, P.lazy(() => T2), BINARY_OPS_PR15);
-
-const T3 = makeBinaryOperationChain(T2, P.lazy(() => T3), BINARY_OPS_PR14);
-
-const EXPR = makeBinaryOperationChain(T3, P.lazy(() => EXPR), BINARY_OPS_PR13);
-
+const EXPR = makeExpressionParser(FACTOR);
 
 export default (tokens) => {
   const parsed = EXPR.parse(tokens);
--- src/operations.js
+++ src/operations.js
@@ -1,49 +1,83 @@
-export const operations = {
-  uMinus: {
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+ *
+ * Operator precedence determines how operators are parsed concerning each other.
+ * Operators with higher precedence become the operands of operators with lower precedence.
+ */
+
+const operations = [
+  {
     type: 'unary',
+    precedence: 17,
+    operators: [
+      {
+        name: 'Unary Negation',
+        operator: '-',
+        fn: (num) => -num,
+      },
+    ],
+  },
+  {
+    type: 'binary',
+    precedence: 14,
+    operators: [
+      {
+        name: 'Addition',
+        operator: '+',
+        fn: (x, y) => x + y,
+      },
+      {
+        name: 'Subtraction',
+        operator: '-',
+        fn: (x, y) => x - y,
+      },
+    ],
+  },
+  {
+    type: 'binary',
     precedence: 16,
-    operator: '-',
-    fn: (num) => -num,
+    operators: [
+      {
+        name: 'Exponentiation',
+        operator: '**',
+        fn: (x, y) => x ** y,
+      },
+    ],
   },
-  add: {
-    type: 'binary',
-    precedence: 13,
-    operator: '+',
-    fn: (x, y) => x + y,
-  },
-  sub: {
-    type: 'binary',
-    precedence: 13,
-    operator: '-',
-    fn: (x, y) => x - y,
-  },
-  pow: {
+  {
     type: 'binary',
     precedence: 15,
-    operator: '**',
-    fn: (x, y) => x ** y,
+    operators: [
+      {
+        name: 'Multiplication',
+        operator: '*',
+        fn: (x, y) => x * y,
+      },
+      {
+        name: 'Division',
+        operator: '/',
+        fn: (x, y) => x / y,
+      },
+    ],
   },
-  mult: {
-    type: 'binary',
-    precedence: 14,
-    operator: '*',
-    fn: (x, y) => x * y,
-  },
-  div: {
-    type: 'binary',
-    precedence: 14,
-    operator: '/',
-    fn: (x, y) => x / y,
-  },
-};
+];
 
-export const getSortedOperationsLexemes = () => Object.keys(operations)
-  .map((op) => operations[op].operator)
+export const getSortedOperationsLexemes = () => operations
+  .reduce((acc, { operators }) => [...acc, ...operators], [])
+  .map(({ operator }) => operator)
   .sort((a, b) => b.length - a.length);
 
-export const getOperationsByType = (type) => Object.keys(operations)
-  .filter((op) => operations[op].type === type)
-  .map((op) => operations[op]);
+// export const getOperationsByType = (opType) => operations
+//   .filter(({ type }) => type === opType)
+//   .map(({ operators }) => operators)
+//   .flat();
 
-export const getBinaryOperationsByPrecedence = (pr) => getOperationsByType('binary')
-  .filter(({ precedence }) => precedence === pr);
+// export const getBinaryOperationsByPrecedence = (pr) => operations
+//   .filter(({ type, precedence }) => type === 'binary' && precedence === pr)
+//   .map(({ operators }) => operators)
+//   .flat();
+
+// export const getOperationsByPrecedence = (pr) => operations
+//   .filter(({ precedence }) => precedence === pr);
+
+export default () => operations;
